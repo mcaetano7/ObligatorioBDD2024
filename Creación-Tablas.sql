3a6021e609e88e3e6588b3ed164df8e13973781a
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-10-26 04:16:42 -0300

    modificacion tablas

--- Creación-Tablas.sql
+++ Creación-Tablas.sql
@@ -45,7 +45,8 @@ CREATE TABLE clase(
     ci_instructor INT,
     id_actividad INT,
     id_turno INT,
-    dictada TINYINT(1) DEFAULT 0,
+    dictada BOOLEAN DEFAULT FALSE,
+    UNIQUE(ci_instructor, id_turno),
     FOREIGN KEY (ci_instructor) REFERENCES instructores(ci),
     FOREIGN KEY (id_actividad) REFERENCES actividades(id),
     FOREIGN KEY (id_turno) REFERENCES turnos(id)
@@ -54,9 +55,12 @@ CREATE TABLE clase(
 CREATE TABLE alumno_clase(
     id_clase INT,
     ci_alumno INT,
+    id_turno INT,
     id_equipamiento INT,
+    UNIQUE(ci_alumno, id_turno),
     PRIMARY KEY (id_clase, ci_alumno),
     FOREIGN KEY (id_clase) REFERENCES clase(id),
     FOREIGN KEY (ci_alumno) REFERENCES alumnos(ci),
+    FOREIGN KEY (id_turno) REFERENCES turnos(id),
     FOREIGN KEY (id_equipamiento) REFERENCES equipamiento(id)
 );
