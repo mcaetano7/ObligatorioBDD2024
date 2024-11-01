4baaeb0166d2cf3be2cd068a38d4cb1d18a04569
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-11-01 12:23:18 -0300

    Inserciones-previas

---
+++ Inserciones-previas.sql
@@ -0,0 +1,25 @@
+USE obligatorio2024;
+
+INSERT INTO turnos(hora_inicio, hora_fin) VALUES
+('9:00', '11:00'),
+('12:00', '14:00'),
+('16:00', '18:00');
+
+INSERT INTO actividades(descripcion, costo) VALUES
+('Snowboard', 2500),
+('Ski', 2900),
+('Moto de Nieve', 4500);
+
+INSERT INTO equipamiento(id_actividad, descripcion, costo) VALUES
+(1, 'Antiparras', 500),
+(2, 'Antiparras', 500),
+(3, 'Antiparras', 500),
+(1, 'Casco', 650),
+(2, 'Casco', 650),
+(3, 'Casco', 650),
+(1, 'Tablas de snowboard', 800),
+(2, 'Esquíes', 750),
+(1, 'Balaclava', 300),
+(2, 'Balaclava', 300),
+(3, 'Balaclava', 300)
+
