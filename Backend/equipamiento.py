025912bd724f6c03d63fc53239d3a126f4fa7609
mateogutierrezo <mateo.gutierrezo@correo.ucu.edu.uy>
2024-11-24 00:37:29 -0300

    Delete Backend/equipamiento.py

--- Backend/equipamiento.py
+++
@@ -1,9 +0,0 @@
-from conexion import conectarse
-
-def obtener_equipamientos(id_actividad):
-    cnx, cursor = conectarse('alumno')
-    cursor.execute("SELECT id, descripcion, costo FROM equipamiento WHERE id_actividad = %s", id_actividad)
-    result = cursor.fetchall()
-    cursor.close()
-    cnx.close()
-    return result
